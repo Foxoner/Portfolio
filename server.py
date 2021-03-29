05f4535d8d1a403091bee2d98f9fa2f625ab5c9e
Foxoner <vadim.avercon@gmail.com>
2021-03-30 02:08:00 +0300

    Finish cod

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, send_from_directory, url_for, request, redirect
 import os
 import csv
+
+
+import smtplib
+
+
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,13 +17,7 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
-# Writing into regular .txt file
-# def write_to_file(data):
-#     with open('../database.txt', mode='a') as database:
-#         email = data['email']
-#         subject = data['subject']
-#         message = data['message']
-#         database.write(f'\n{email},{subject},{message}')
+
 
 def write_to_csv(data):
     with open('../database.csv', mode='a', newline='') as database2:
@@ -32,20 +32,23 @@ def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
+            #Start Email
+            msg = 'Email - {}; Subject - {}; Message - {}'.format(data['email'],data['subject'],data['message'])
+            with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+                smtp.ehlo()
+                smtp.starttls()
+                smtp.login('vadim.avercon@gmail.com', '452263632')
+                smtp.sendmail('vadim.avercon@gmail.com','vadim.avercon@gmail.com', msg)
+                smtp.quit()
+            # End Email
             print(data)
             write_to_csv(data)
             return 'Thank you!' #redirect('/thankyou.html')
+
         except:
             return 'Didi not save to database!'
     else:
         return 'Something went wrong. Try again!'
 
 
-
-# One more way to add a favicon
-
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#     'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
